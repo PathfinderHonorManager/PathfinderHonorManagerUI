e1c5e623a97768f9f13857c6decf9e1e2aa7feae
John Berger <jberger@cardlytics.com>
2022-04-08 12:14:46 -0400

    clean up pathfinders api

--- src/api/pathfinders.ts
+++ src/api/pathfinders.ts
@@ -1,39 +1,19 @@
 import axios from "axios";
-import type createAuth0Client from "@auth0/auth0-spa-js";
-import type { Auth0Client, GetTokenSilentlyOptions } from "@auth0/auth0-spa-js";
-import { ref } from "vue";
 
 // I find it easier to set up an api call file for each endpoint,
 // It's easier to read and understand.
 
-const auth0Client = ref<Auth0Client | null>(null);
-
 const BASE_URL =
   "https://pathfinderhonormanager.azurewebsites.net/api/pathfinders";
-
-const getAccessToken = async (
-  options?: GetTokenSilentlyOptions
-): Promise<null | string> => {
-  if (!auth0Client.value) {
-    return null;
-  }
-
-  return (await auth0Client.value.getTokenSilently(options)) as string;
-};
-
-const token = await getAccessToken();
 
 export default {
   getAll: (params = {}) => {
     return axios.get(BASE_URL, {
-      params: params,
-      headers: {
-        Authentication: "Bearer " + token,
-      },
+      params,
     });
   },
   get: (id: number, params = {}) => {
-    // I assume you can retreive pathfinders by id
+    // I assume you can retrieve pathfinders by id
     return axios.get(BASE_URL + `/${id}`, {
       params,
     });
