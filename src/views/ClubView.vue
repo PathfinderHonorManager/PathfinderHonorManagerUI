a4e72be6dbc95776b0d669f5d3ebc2e7a47e16c1
sardine-craft <65100242+sardine-craft@users.noreply.github.com>
2022-12-20 14:36:07 -0500

    Error Handling (post pathfinder)

--- src/views/ClubView.vue
+++ src/views/ClubView.vue
@@ -70,6 +70,8 @@ import { defineComponent } from "vue";
 import { usePathfinderStore } from "../stores/pathfinders";
 import { ArgumentPlaceholder } from "@babel/types";
 
+import { Errors } from "../errors/errors";
+
 const pathfinderStore = usePathfinderStore();
 
 export default defineComponent({
@@ -88,6 +90,20 @@ export default defineComponent({
         email: getRefValue("email"),
         grade: Number(getRefValue("grade")),
       };
+
+      if (data.firstName === "") {
+        throw Errors.postFormData.invalidFirstName;
+      }
+      if (data.lastName === "") {
+        throw Errors.postFormData.invalidLastName;
+      }
+      if (data.email === "") {
+        throw Errors.postFormData.invalidEmail;
+      }
+      if (data.grade < 5 || data.grade > 12 || data.grade != 0) {
+        throw Errors.postFormData.invalidGrade;
+      }
+
       pathfinderStore.postPathfinder(data);
     },
   },
