4dba1d9d52c5e3f9fecf94b08ed0317aaf0801b8
Brian Cummings <brian.cummings@gmail.com>
2024-04-02 11:53:15 -0400

    Broke the search component out

---
+++ src/components/HonorSearchComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <input
+        id="honorform"
+        @keyup="$emit('search-query', $event.target.value)"
+        type="text"
+        placeholder="Search. . . "
+      />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HonorSearchComponent",
+  };
+  </script>
--- src/components/PlanHonorsComponent.vue
+++ src/components/PlanHonorsComponent.vue
@@ -2,91 +2,87 @@
   <div class="outline" style="text-align: center">
     <h3>Find and Add Honors To Your Club</h3>
 
-    <input
-      id="honorform"
-      @keyup="doHonorSearch"
-      type="text"
-      placeholder="Search. . . "
-    />
-  </div>
+    <HonorSearchComponent @search-query="doHonorSearch" />
 
-  <span class="loader" v-if="loading">Loading Honors</span>
-  <div v-if="honorSearchResult.length > 0">
-    <h3>Search Results</h3>
-    <HonorsDisplayComponent
-      :honorSearchResult="honorSearchResult"
-      :isSelected="isSelected"
-      @toggle-selection="toggleSelection"
-    />
+    <span class="loader" v-if="loading">Loading Honors</span>
+    <div v-if="honorSearchResult.length > 0">
+      <h3>Search Results</h3>
+      <HonorsDisplayComponent
+        :honorSearchResult="honorSearchResult"
+        :isSelected="isSelected"
+        @toggle-selection="toggleSelection"
+      />
  
-    <h3>Selected Honors</h3>
-    <SelectedHonorsDisplayComponent
-      :selectedHonors="selectedHonors"
-      @toggle-selection="toggleSelection"
-    />
-    <h3>Recipients</h3>
-    <div class="outline" style="display: flex">
-      <button
-        v-for="(recipient, i) in pathfinders"
-        :key="i"
-        class="button"
-        :style="{
-          backgroundColor: pathfinderHasSelectedHonor(recipient.pathfinderID)
-            ? 'var(--red)'
-            : pathfinderStore.isSelected(recipient.pathfinderID)
-              ? 'var(--actionColor)'
-              : 'var(--lightGrey)',
-          flexGrow: 1,
-        }"
-        @click="
-          pathfinderHasSelectedHonor(recipient.pathfinderID)
-            ? null
-            : toggleRecipientSelection(recipient.pathfinderID)
-        "
-        :title="
-          pathfinderHasSelectedHonor(recipient.pathfinderID)
-            ? 'This pathfinder already has the selected honor'
-            : ''
-        "
-      >
-        {{ recipient.firstName }} {{ recipient.lastName }}
-      </button>
+      <h3>Selected Honors</h3>
+      <SelectedHonorsDisplayComponent
+        :selectedHonors="selectedHonors"
+        @toggle-selection="toggleSelection"
+      />
+      <h3>Recipients</h3>
+      <div class="outline" style="display: flex">
+        <button
+          v-for="(recipient, i) in pathfinders"
+          :key="i"
+          class="button"
+          :style="{
+            backgroundColor: pathfinderHasSelectedHonor(recipient.pathfinderID)
+              ? 'var(--red)'
+              : pathfinderStore.isSelected(recipient.pathfinderID)
+                ? 'var(--actionColor)'
+                : 'var(--lightGrey)',
+            flexGrow: 1,
+          }"
+          @click="
+            pathfinderHasSelectedHonor(recipient.pathfinderID)
+              ? null
+              : toggleRecipientSelection(recipient.pathfinderID)
+          "
+          :title="
+            pathfinderHasSelectedHonor(recipient.pathfinderID)
+              ? 'This pathfinder already has the selected honor'
+              : ''
+          "
+        >
+          {{ recipient.firstName }} {{ recipient.lastName }}
+        </button>
+      </div>
+      <p>{{ recipients.length }} recipients selected</p>
+      <p v-if="recipients.length == 1" class="note">
+        When planning honors for individuals, we recommend doing it in the My Club
+        page.
+      </p>
     </div>
-    <p>{{ recipients.length }} recipients selected</p>
-    <p v-if="recipients.length == 1" class="note">
-      When planning honors for individuals, we recommend doing it in the My Club
-      page.
-    </p>
-  </div>
 
-  <div class="content-box center-align">
-    <button @click="addSelectedToClub" class="primary button">
-      Plan Selected ({{ selected.length }})
-    </button>
-    <p class="note">
-      This will add your selection of honors as a planned honor for every
-      selected member in your club.
-    </p>
-  </div>
+    <div class="content-box center-align">
+      <button @click="addSelectedToClub" class="primary button">
+        Plan Selected ({{ selected.length }})
+      </button>
+      <p class="note">
+        This will add your selection of honors as a planned honor for every
+        selected member in your club.
+      </p>
+    </div>
 
-  <ToasterComponent
-    v-if="bulkAdd"
-    message="Your honors have been added to your club"
-  />
+    <ToasterComponent
+      v-if="bulkAdd"
+      message="Your honors have been added to your club"
+    />
+  </div>
 </template>
 
-<script lang="ts">
+<script>
 import ToasterComponent from "./ToasterComponent.vue";
-
-import { defineComponent, ref, inject } from "vue";
-import { storeToRefs } from "pinia";
+import HonorSearchComponent from './HonorSearchComponent.vue';
 import HonorsDisplayComponent from './HonorsDisplayComponent.vue';
 import SelectedHonorsDisplayComponent from './SelectedHonorsDisplayComponent.vue';
 
+import { defineComponent, ref, inject } from "vue";
+import { storeToRefs } from "pinia";
 
 export default defineComponent({
   components: {
     ToasterComponent,
+    HonorSearchComponent,
     HonorsDisplayComponent,
     SelectedHonorsDisplayComponent,
   },
@@ -111,14 +107,6 @@ export default defineComponent({
 
     let recipients = ref(pathfinderStore.getPathfindersBySelection());
     let bulkAdd = ref(false);
-
-    function doHonorSearch(event) {
-      selectedHonors.value = honorStore.getHonorsBySelection();
-      honorSearchResult.value =
-        event.target.value !== ""
-          ? honorStore.getHonorsByQuery(event.target.value).splice(0, 15)
-          : [];
-    }
 
     async function addSelectedToClub() {
       bulkAdd.value = true;
@@ -152,6 +140,12 @@ export default defineComponent({
       return pathfinder.pathfinderHonors.some((h) =>
         selected.value.includes(h.honorID),
       );
+    }
+
+    function doHonorSearch(searchQuery) {
+      honorSearchResult.value = searchQuery !== ""
+        ? honorStore.getHonorsByQuery(searchQuery)
+        : [];
     }
 
     return {
