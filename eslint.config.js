9ece589e146df7ae0813928dd67ccfcb55994b01
Brian Cummings <brian.cummings@gmail.com>
2025-05-27 20:43:47 -0400

    Fixed linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import pluginVue from 'eslint-plugin-vue'
+import tseslint from '@vue/eslint-config-typescript'
+
 export default [
-
   ...pluginVue.configs['flat/recommended'],
-
+  ...tseslint(),
   {
     rules: {
-
+      'vue/require-explicit-emits': 'warn',
+      'vue/require-default-prop': 'warn'
     }
   }
 ]
