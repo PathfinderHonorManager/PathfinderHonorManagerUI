459ea1c93118c6900290119721a3901d820afd7b
Brian Cummings <brian.cummings@gmail.com>
2025-06-03 17:40:29 -0400

    Added coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default mergeConfig(
       globals: true,
       coverage: {
         provider: 'v8',
-        reporter: ['text', 'json', 'html'],
+        reporter: ['text', 'json', 'html', 'lcov'],
       },
     },
   })
